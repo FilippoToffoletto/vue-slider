3a378eced699a663305ce79762e6a90812cc9413
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-03 16:10:31 +0100

    [add]autoplay and btns function

--- js/vue.js
+++ js/vue.js
@@ -17,7 +17,57 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            
+            photos:[
+                '../img/01.jpg',
+                '../img/02.jpg',
+                '../img/03.jpg',
+                '../img/04.jpg',
+                '../img/05.jpg',
+            ],
+            counterPhoto: 0
+        }
+    },
+    methods:{
+        nextPhoto(){
+          this.counterPhoto++;
+          // se il contatore è più alto dell'ultimo elemento dell'array
+          // lo resetto a 0
+          if(this.counterPhoto > this.photos.length-1){
+            this.counterPhoto = 0;
+          }
+          console.log(this.counterPhoto);
+        },
+        prevPhoto(){
+          this.counterPhoto--;
+          // se il contatore  inferiore a 0
+          // lo faccio puntare all'ultimo elemnto dell'array
+          if(this.counterPhoto < 0){
+            this.counterPhoto = this.photos.length -1;
+          }
+          console.log(this.counterPhoto);
+        }
+      },
+      created(){
+        // questo metodo viene scatenato quando l'app è pronta ma non ho ancora letto il DOM
+        // qui metto le funzioni che partono di default
+        console.log('APP CREATA')
+    },
+    created(){
+        // questo metodo viene scatenato quando l'app è pronta ma non ho ancora letto il DOM
+        // qui metto le funzioni che partono di default
+        console.log('APP CREATA')
+    },
+    mounted(){
+        // questo metodo viene scatenato quando l'app è pronta
+        // qui metto le funzioni che partono di default
+        console.log('APP MONTATA')
+        setInterval( () => {
+            console.log('--> setInterval', this);
+            this.nextPhoto();
+          }, 3000)
+        for (let i = 0; i < this.photos.length; i++) {
+            const element = this.photos[i];
         }
     }
 }).mount('#app');
+
