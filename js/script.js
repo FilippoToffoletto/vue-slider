2e644493b048a9a742508861665cc021242cdef4
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-11-03 17:48:00 +0100

    final

--- js/script.js
+++ js/script.js
@@ -1,109 +1,139 @@
-const arrayImg = [
-    '01.jpg',
-    '02.jpg',
-    '03.jpg',
-    '04.jpg',
-    '05.jpg',
-]
-  
-// "CHIAMO" I MIEI CONTENITORI DELLE IMG
-const containerImgLg = document.querySelector('.img-lg');
-const containerImgXs = document.querySelector('.img-xs');
-  
-// CREO LE COSTANTI CHE POI USERO' PER INTERAGIRE CON L'HTML
-let imgLg = "";
-let imgXs = "";
-  
-// CREO IL CONTATORE PER LA GESTIONE DELLE CLASSI
-let counterImg = 0;
-  
- 
-// "SCOMPONGO" L'ARRAY USANDO IL CICLO FOR E STAMPO I TAG CHE MI SERVONO
-for(let i = 0; i < arrayImg.length; i++){
-    //CREO IL TAG IMG PER IL BOX XL
-    imgLg = `
-    <img class="xl hide" src="img/${arrayImg[i]}" alt="${arrayImg[i]}">
-    `;
-    //CREO IL TAG IMG PER IL BOX XS (THUMBNAILS)
-    imgXs = `
-    <img class="xs" src="img/${arrayImg[i]}" alt="${arrayImg[i]}">
-    `;
-    
-    // STAMPO I TAG CREATI NEI BOX NEL HTML
-    containerImgLg.innerHTML += imgLg;
-    containerImgXs.innerHTML += imgXs;
-}
-  
-// PRENDO IL COINTAINER CHE UTILIZZERO' PER L'OVER
-const carousel = document.querySelector('.carousel');
-  
-// CREO LA SITUAZIONE DI DEFAULT CHE MI OCCORRE:
-// PRENDO I CONTENITORI CHE MI SERVIRANNO
-const arrayImgLg = document.getElementsByClassName('xl');
-const arrayImgXs = document.getElementsByClassName('xs');
-  
-// AGGIUNGO E TOLGO CLASSI PER AVERE LA VISUALIZZAZIONE DI DEFAULT
-arrayImgLg[counterImg].classList.remove('hide');
-arrayImgLg[counterImg].classList.add('active');
-  
-arrayImgXs[counterImg].classList.add('active');
-  
-// CREO DUE COSTANTI CON I MIEI BOTTONI PER SCROLLARE LE IMMAGINI
-const up = document.querySelector('.btn.up');
-const down = document.querySelector('.btn.down');
-
-// do un intervallo per il cambio immagini e una variabile globale
-let changeTime = 1.7 ;
-let startInterval;
-
-// prendo il container con le immagini di sx
-const stopInterval = document.querySelector('.carousel');
-  
-// loop che cambia le immagini
-startInterval = setInterval(function(){
-  upDown(true);
-}, changeTime * 1000)
-
-// fermo l'intervallo con l'hover del mouse sul container
-stopInterval.addEventListener('mouseover', function(){
-    clearInterval(startInterval);
-})
-// faccio ripartire l'intervallo con l'hover del mouse sul container
-stopInterval.addEventListener('mouseout', function(){
-    startInterval = setInterval(function(){
-        upDown(true);
-      }, changeTime * 1000)  
-})
-
-
-  
-down.addEventListener('click',function(){
-    upDown(true);
-})
-  
-up.addEventListener('click', function (){
-    upDown(false);
-})
-  
-  
-  
-function upDown(isDown){
-    arrayImgLg[counterImg].classList.remove('active');
-    arrayImgLg[counterImg].classList.add('hide');
-  
-    arrayImgXs[counterImg].classList.remove('active');
-  
-    if(isDown){
-      counterImg++;
-      if(counterImg === arrayImg.length) counterImg = 0;
-    }else {
-      counterImg--;
-      if(counterImg < 0) counterImg = arrayImg.length -1;
+const postiDaVisitare = [
+    {
+      nome: "Montebelluna",
+      descrizione: "Il toponimo è chiaramente un composto. Monte- indicherebbe la collina di Mercato Vecchio, ai piedi della quale è sorto l'abitato. Più discussa l'origine di -belluna: potrebbe essere in relazione al culto della dea Bellona; o, posticipandone l'origine, si richiamerebbe alla città di Belluno che, nel X secolo, aveva espanso la propria giurisdizione fin oltre il Piave grazie alle conquiste del vescovo Giovanni",
+      urlImg: 'montebelluna.jpg'
+    },
+    {
+      nome: "Restera Treviso",
+      descrizione: "La Restera è la strada che corre lungo gli argini del Sile,In passato era il camminamento per uomini ed animali addetti al traino dei burci.I burci erano pesanti imbarcazioni destinate al trasporto di granaglie, inerti e laterizi che in questo tratto del fiume dovevano venire trascinate fino al vicino porto di Fiera.",
+      urlImg: 'restera.JPG'
+    },
+    {
+      nome: "Sacile",
+      descrizione: "In epoca romana il territorio di Sacile rientrava nella giurisdizione del municipio di Oderzo, ma non è provata l'esistenza di un vero centro abitato. Dopo la distruzione di Oderzo da parte dei Longobardi di re Grimoaldo, la zona assunse importanza strategica in quanto localizzata nel punto in cui la strada che collegava Pavia (capitale del regno longobardo) a Cividale superava il Livenza tramite un ponte; in aggiunta, il fiume era divenuto un confine naturale tra i neoistituiti ducati di Ceneda e del Friuli. ",
+      urlImg: 'sacile.jpg'
+    },
+    {
+      nome: "Treviso",
+      descrizione: "La prima menzione di Treviso, seppur indiretta, compare nel III libro della Naturalis historia di Plinio il Vecchio in cui è citato il «Fluvius Silis ex montibus Tarvisanis». Bisognerà aspettare il De vita sancti Martini di Venanzio Fortunato per avere una prima citazione del toponimo «Tarvisus», seguito, poco dopo, dall'Anonimo Ravennate con «Trabision». Numerosi sono poi i riferimenti nell'Historia Langobardorum di Paolo Diacono: «Tribicium seu Tarbision», «apud Tarvisium» ecc. L'ipotesi più probabile è che Tarvisium, scomponibile in Tarv-is-ium, sia di origine celtica: si riconoscono infatti tarvos e la formante -is- tipica dei toponimi gallici.",
+      urlImg: 'treviso.jpg'
+    },
+    {
+      nome: "Venezia",
+      descrizione: "La città è stata per 1100 anni la capitale della Serenissima Repubblica di Venezia ed è conosciuta a questo riguardo come la Serenissima, la Dominante e la Regina dell'Adriatico: per le peculiarità urbanistiche e per il suo patrimonio artistico, è universalmente considerata una tra le più belle città del mondo, dichiarata, assieme alla sua laguna, patrimonio dell'umanità dall'UNESCO, che ha contribuito a farne la seconda città italiana dopo Roma con il più alto flusso turistico.",
+      urlImg: 'venice.jpeg'
     }
+];
+// 1 richiamo tag
+const containerImgLg = document.querySelector('.container-slider-top');
+const containerImgXs = document.querySelector('.container-slider-bottom');
+const buttonRight = document.querySelector('.btn.right');
+const buttonLeft = document.querySelector('.btn.left');
+const buttonShuffle = document.querySelector('.shuffle');
+const buttonStop = document.querySelector('.stop');
+
+// creo l'html dinamicamente
+postiDaVisitare.forEach(luogo => {
+  createContentTop(luogo);
+  createContentBottom(luogo);
+});
+
+// creo la situazione di default
+
+document.querySelector('.img-slider-top').classList.remove('d-none');
+document.querySelector('.img-slider-bottom').classList.add('active');
+let counter = 0;
+let timing;
+let timeChange = 5;
+
+// creo i due arrey contenenti la mia collezione di immagini
+const arrayImgTop = document.getElementsByClassName('img-slider-top');
+const arrayImgBottom = document.getElementsByClassName('img-slider-bottom');
+let isLeft;
+let isShuffle;
+
+timingCarousel();
+
+buttonLeft.addEventListener('click', function(){
+  isLeft = true;
+  sliderNextPrev(isLeft,arrayImgTop,arrayImgBottom );
+})
+buttonRight.addEventListener('click', function(){
+  isLeft = false;
+  sliderNextPrev(isLeft,arrayImgTop,arrayImgBottom );
+})
+buttonShuffle.addEventListener('click', function(){
+  let isShuffle = true;
+  shuffle(isShuffle);
+  buttonShuffle.innerHTML = 'INVERTI';
+});
+buttonStop.addEventListener('click', function(){
+  clearInterval(timing);
+  buttonShuffle.innerHTML = 'START';
+})
+
+
+//FUNZIONI
+function createContentTop(oggetto){
+ let contentTop = '';
+ 
+  contentTop = `
+  <div class="img-slider-top d-none">
+    <img src="img/${oggetto.urlImg}" alt="${oggetto.nome}"> 
   
-    arrayImgLg[counterImg].classList.remove('hide');
-    arrayImgLg[counterImg].classList.add('active');
+    <div class="description">
+      <h2> ${oggetto.nome}  </h2>
+      <p>  ${oggetto.descrizione} </p>
+    </div>
+  </div>  
+`;
+
+containerImgLg.innerHTML += contentTop;
+};
+
+function createContentBottom(oggetto){
+let contentBottom = '';
+
+
+contentBottom = `
+  <div class="img-slider-bottom">
+    <img class="" src="img/${oggetto.urlImg}" alt="${oggetto.nome}">
+  </div>
+`;
+
+containerImgXs.innerHTML += contentBottom;
+};
+
+function sliderNextPrev(flag,array1, array2){
+  array1[counter].classList.add('d-none');
+  array2[counter].classList.remove('active');
+
+  if(!(flag)) {
+    ++counter
+    if(counter == postiDaVisitare.length) counter = 0;
+  } else {
+    if(counter == 0) counter = postiDaVisitare.length;
+    --counter
+  }
   
-    arrayImgXs[counterImg].classList.add('active')
+  array1[counter].classList.remove('d-none');
+  array2[counter].classList.add('active')
 }
-  
+
+// timing function
+function timingCarousel(flag){
+ timing = setInterval(function(){
+    sliderNextPrev(flag, arrayImgTop, arrayImgBottom)
+  }, timeChange * 1000);
+}
+
+function shuffle(flag){
+  let isChange;
+  if(flag) isChange = true;
+  else{
+    isChange = false;
+  }
+  clearInterval(timing)
+  timingCarousel(isChange);
+}
--- js/vue.js
+++ js/vue.js
@@ -1,73 +1,60 @@
-/*
-Descrizione:
-Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
-- al click su una thumb, visualizzare in grande l’immagine corrispondente
-- aggiungere la classe active alla thumb attiva
-Bonus:
-1- applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-2- quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
-Consigli del giorno:
-- regola d’oro: riciclare ovunque possibile! Questo significa che per la parte di markup possiamo recuperare html e css dell’esercizio svolto qualche giorno fa: è già tutto pronto!
-- il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l’esercizio si riduce a poche righe ;)
-Buon lavoro e buon divertimento!
-*/
-
 const {createApp} = Vue;
 
 createApp({
     data(){
         return{
-            photos:[
-                '../img/01.jpg',
-                '../img/02.jpg',
-                '../img/03.jpg',
-                '../img/04.jpg',
-                '../img/05.jpg',
+            postiDaVisitare: [
+                {
+                  nome: "Montebelluna",
+                  descrizione: "Il toponimo è chiaramente un composto. Monte- indicherebbe la collina di Mercato Vecchio, ai piedi della quale è sorto l'abitato. Più discussa l'origine di -belluna: potrebbe essere in relazione al culto della dea Bellona; o, posticipandone l'origine, si richiamerebbe alla città di Belluno che, nel X secolo, aveva espanso la propria giurisdizione fin oltre il Piave grazie alle conquiste del vescovo Giovanni",
+                  urlImg: 'montebelluna.jpg'
+                },
+                {
+                  nome: "Restera Treviso",
+                  descrizione: "La Restera è la strada che corre lungo gli argini del Sile,In passato era il camminamento per uomini ed animali addetti al traino dei burci.I burci erano pesanti imbarcazioni destinate al trasporto di granaglie, inerti e laterizi che in questo tratto del fiume dovevano venire trascinate fino al vicino porto di Fiera.",
+                  urlImg: 'restera.JPG'
+                },
+                {
+                  nome: "Sacile",
+                  descrizione: "In epoca romana il territorio di Sacile rientrava nella giurisdizione del municipio di Oderzo, ma non è provata l'esistenza di un vero centro abitato. Dopo la distruzione di Oderzo da parte dei Longobardi di re Grimoaldo, la zona assunse importanza strategica in quanto localizzata nel punto in cui la strada che collegava Pavia (capitale del regno longobardo) a Cividale superava il Livenza tramite un ponte; in aggiunta, il fiume era divenuto un confine naturale tra i neoistituiti ducati di Ceneda e del Friuli. ",
+                  urlImg: 'sacile.jpg'
+                },
+                {
+                  nome: "Treviso",
+                  descrizione: "La prima menzione di Treviso, seppur indiretta, compare nel III libro della Naturalis historia di Plinio il Vecchio in cui è citato il «Fluvius Silis ex montibus Tarvisanis». Bisognerà aspettare il De vita sancti Martini di Venanzio Fortunato per avere una prima citazione del toponimo «Tarvisus», seguito, poco dopo, dall'Anonimo Ravennate con «Trabision». Numerosi sono poi i riferimenti nell'Historia Langobardorum di Paolo Diacono: «Tribicium seu Tarbision», «apud Tarvisium» ecc. L'ipotesi più probabile è che Tarvisium, scomponibile in Tarv-is-ium, sia di origine celtica: si riconoscono infatti tarvos e la formante -is- tipica dei toponimi gallici.",
+                  urlImg: 'treviso.jpg'
+                },
+                {
+                  nome: "Venezia",
+                  descrizione: "La città è stata per 1100 anni la capitale della Serenissima Repubblica di Venezia ed è conosciuta a questo riguardo come la Serenissima, la Dominante e la Regina dell'Adriatico: per le peculiarità urbanistiche e per il suo patrimonio artistico, è universalmente considerata una tra le più belle città del mondo, dichiarata, assieme alla sua laguna, patrimonio dell'umanità dall'UNESCO, che ha contribuito a farne la seconda città italiana dopo Roma con il più alto flusso turistico.",
+                  urlImg: 'venice.jpeg'
+                }
             ],
-            counterPhoto: 0
+            counterImg: 0,
+            autoPlay: '',
+            isStop: true
         }
     },
-    methods:{
-        nextPhoto(){
-          this.counterPhoto++;
-          // se il contatore è più alto dell'ultimo elemento dell'array
-          // lo resetto a 0
-          if(this.counterPhoto > this.photos.length-1){
-            this.counterPhoto = 0;
-          }
-          console.log(this.counterPhoto);
+    methods: {
+        nextPrev(isNext){
+          isNext ? this.counterImg++ : this.counterImg--;
+          if(this.counterImg === this.postiDaVisitare.length) this.counterImg = 0;
+    
+          if(this.counterImg < 0) this.counterImg = this.postiDaVisitare.length - 1;
         },
-        prevPhoto(){
-          this.counterPhoto--;
-          // se il contatore  inferiore a 0
-          // lo faccio puntare all'ultimo elemnto dell'array
-          if(this.counterPhoto < 0){
-            this.counterPhoto = this.photos.length -1;
-          }
-          console.log(this.counterPhoto);
+        autoScroll(){
+          this.autoPlay = setInterval(() => {
+            this.nextPrev(true);
+          }, 3000);
+        },
+        stopAutoScroll(){
+          clearInterval(this.autoPlay);
+        },
+        checkIsStopped(){
+          this.isStop ? this.isStop = !this.isStop : this.isStop = !this.isStop;
         }
-      },
-      created(){
-        // questo metodo viene scatenato quando l'app è pronta ma non ho ancora letto il DOM
-        // qui metto le funzioni che partono di default
-        console.log('APP CREATA')
-    },
-    created(){
-        // questo metodo viene scatenato quando l'app è pronta ma non ho ancora letto il DOM
-        // qui metto le funzioni che partono di default
-        console.log('APP CREATA')
     },
     mounted(){
-        // questo metodo viene scatenato quando l'app è pronta
-        // qui metto le funzioni che partono di default
-        console.log('APP MONTATA')
-        setInterval( () => {
-            console.log('--> setInterval', this);
-            this.nextPhoto();
-          }, 3000)
-        for (let i = 0; i < this.photos.length; i++) {
-            const element = this.photos[i];
-        }
+        this.autoScroll();
     }
 }).mount('#app');
-
